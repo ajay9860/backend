e47efb79acbf942719fc488a068d6b26fee5bda1
ajay9860 <kshirsagar1508@gmail.com>
2024-07-14 21:34:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express')
+//const colors = express('colors')
+const morgan = require('morgan')
+const dotenv = require('dotenv');
+const mySqlPool = require('./config/db');
+const cors = require('cors');
+
+const corsOptions = {
+    origin: 'http://localhost:8005', // Allow only requests from this origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow these HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-access-token'], // Allow these headers
+    credentials: true, // Allow cookies to be sent
+  };
+// config dotenv
+
+dotenv.config();
+
+// rest object
+
+const app = express()
+
+// middlewares
+app.use(express.json())
+
+app.use(morgan("dev"));
+
+// routes
+
+app.use(cors(corsOptions));
+
+app.use('/api/v1/myproject', require('./routes/userRoutes'));
+
+app.get('/test', (req,res) => {
+    res.status(200).send('<h1>1aaamy sqy</h1>')
+})
+
+
+// sport
+
+const port = process.env.port || 8000
+
+mySqlPool.query('SELECT 1').then(() => {
+
+    console.log('Database connected')
+
+    // listen
+    app.listen(port, () => {
+        console.log(`server listen on port ${process.env.port}`)
+    })
+}).catch((error) => {
+    console.log(error);
+})
